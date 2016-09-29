9a2054ca47218eff6f5b7c9fb460090272dcd16d
Komaron <kjames953@gmail.com>
2016-09-28 21:27:27 -0400

    Creating Topic model and building app

--- learning_blogs/admin.py
+++ learning_blogs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from learning_blogs.models import Topic
+
+admin.site.register(Topic)
--- learning_blogs/models.py
+++ learning_blogs/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+	'''A given topic a user wants to blog about'''
+	text = models.CharField(max_length=100)
+	date_uploaded = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.text
